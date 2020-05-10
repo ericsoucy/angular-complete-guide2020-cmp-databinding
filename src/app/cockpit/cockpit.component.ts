b3dc1b6fe524d99564dc12fc61be93669a741388
eric soucy <eric.soucy@gmail.com>
2020-05-10 16:58:11 -0400

    ch69

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -12,7 +12,7 @@ export class CockpitComponent implements OnInit {
     serverContent: string;
   }>();
 
-  @Output()
+  @Output('bpCreated')
   blueprintCreated = new EventEmitter<{
     serverName: string;
     serverContent: string;
