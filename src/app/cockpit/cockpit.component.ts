4b9c551fa5b1d994f0728778a3f869dc9abfc0ee
eric soucy <eric.soucy@gmail.com>
2020-05-11 18:54:21 -0400

    ch75 view child

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  EventEmitter,
+  Output,
+  ViewChild,
+  ElementRef,
+} from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -19,24 +26,26 @@ export class CockpitComponent implements OnInit {
   }>();
 
   // newServerName = '';
-  newServerContent = '';
+  // newServerContent = '';
+
+  @ViewChild('serverContentInput')
+  serverContentInput: ElementRef;
 
   constructor() {}
 
   ngOnInit(): void {}
 
   onAddServer(nameInput: HTMLInputElement) {
-    console.log(nameInput.value);
     this.serverCreated.emit({
       serverName: nameInput.value,
-      serverContent: this.newServerContent,
+      serverContent: this.serverContentInput.nativeElement.value,
     });
   }
 
   onAddBlueprint(nameInput: HTMLInputElement) {
     this.blueprintCreated.emit({
       serverName: nameInput.value,
-      serverContent: this.newServerContent,
+      serverContent: this.serverContentInput.nativeElement.value,
     });
   }
 }
